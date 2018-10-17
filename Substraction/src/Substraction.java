ab207a20b5ae6ded86d1ba02cccda74ff4be4864
etu-20 <etu-20@ETU-20>
2018-10-17 12:12:04 +0530

    Initial COmmit

---
+++ Substraction/src/Substraction.java
@@ -0,0 +1,11 @@
+
+public class Substraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=30, b=20, c=0;
+		c= a - b;
+		System.out.println("Substraction is :"+c);
+	}
+
+}
